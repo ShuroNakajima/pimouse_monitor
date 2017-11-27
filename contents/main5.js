3521c3d2e110f1b7b3b8bc16430da4b1ce489687
Shuro Nakajima <Shuro Nakajima>
2017-11-28 04:31:15 +0900

    add complete controller

--- contents/main5.js
+++ contents/main5.js
@@ -46,4 +46,31 @@ document.getElementById('motor_off').addEventListener('click', function(e){
     });
 });
 
+var vel = new ROSLIB.Topic({
+    ros : ros,
+    name : '/cmd_vel',
+    messageType : 'geometry_msgs/Twist'
+});
+
+function pubMotorValues(){
+    fw = $('#vel_fw').html();
+    rot = $('#vel_rot').html();
+
+    fw = parseInt(fw)*0.001;
+    rot = 3.141592*parseInt(rot)/180;
+    v = new ROSLIB.Message({linear:{x:fw,y:0,z:0}, angular:{x:0,y:0,z:rot}});
+    vel.publish(v);
+}
+
+$('#touchmotion').on('click',function(e){
+    rect = $('#touchmotion')[0].getBoundingClientRect();
+    x = e.pageX - rect.left - window.pageXOffset;
+    y = e.pageY - rect.top - window.pageYOffset;
+
+    vel_fw = (rect.height/2 - y)*3;
+    vel_rot = rect.width/2 - x;
+    $('#vel_fw').html(parseInt(vel_fw));
+    $('#vel_rot').html(parseInt(vel_rot));
+});
+
 document.getElementById('camstream').data = 'http://' + location.hostname + ':10000/stream?topic=/cv_camera_node/image_raw';
